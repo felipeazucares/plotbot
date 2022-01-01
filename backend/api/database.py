a1be3fc68feb7ec98d5890617e9a10f03acafdc1
FelipeAzucares <phil@red-robot.biz>
2022-01-01 20:50:49 +0000

    Working: ! tody up debug lines added to add_a_node routine

--- backend/api/database.py
+++ backend/api/database.py
@@ -44,7 +44,9 @@ class UserStorage:
                 message_to_show=f"get_user_details_by_username({self.username}) called"
             )
         try:
-            user_deets = await self.user_collection.find_one({"username": self.username})
+            user_deets = await self.user_collection.find_one(
+                {"username": self.username}
+            )
             if user_deets is not None:
                 self.user_details = UserDetails(**user_deets)
             else:
@@ -211,7 +213,9 @@ class StoryStorage:
                 message_to_show=f"return_latest_story({self.user_id}) called"
             )
         try:
-            self.last_save = await self.return_latest_save_document(user_id=self.user_id)
+            self.last_save = await self.return_latest_save_document(
+                user_id=self.user_id
+            )
         except Exception as exception_object:
             self.console_display.show_exception_message(
                 message_to_show=f"Exception occured retrieving latest save from the database user_id was: {self.user_id}"
@@ -240,7 +244,6 @@ class StoryStorage:
                 )
             try:
                 self.tree = Tree()
-                self.tree.show()
             except Exception as exception_object:
                 self.console_display.show_exception_message(
                     message_to_show="Exception occured creating new tree"
@@ -269,7 +272,7 @@ class StoryStorage:
             self.console_display.show_debug_message(
                 message_to_show=f"story root node {self.last_save_story.root}"
             )
-        self.last_save_story.show()
+        # self.last_save_story.show()
         if self.last_save_story.root is not None:
             if DEBUG:
                 self.console_display.show_debug_message(
@@ -439,8 +442,8 @@ class StoryStorage:
             )
             print(exception_object)
             raise
+
         # parent (_predecessor) and children(_successors) are stored under against self.tree_id key
-        print(f"tree_id:{self.tree_id}")  # tree id is corrupted at this point somehow
         if DEBUG:
             self.console_display.show_debug_message(
                 message_to_show=f"{self.node_id} _successors: {self.loaded_tree['_nodes'][self.node_id]['_successors']}"
@@ -508,7 +511,9 @@ class StoryStorage:
         if self.children is not None:
 
             if DEBUG:
-                self.console_display.show_debug_message(message_to_show="recursive call")
+                self.console_display.show_debug_message(
+                    message_to_show="recursive call"
+                )
             for self.child_id in self.children:
                 self.add_a_node(
                     tree_id=self.tree_id,
