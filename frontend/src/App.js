b7f4db2ec7abf1d9c59766f36fef933eaa3c41c2
Philip Suggars <phil@red-robot.biz>
2022-01-03 10:53:10 +0000

    WIP: created default tree component with dummy data

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import logo from "./logo.svg";
+import "./App.css";
+import OrgChart from "./components/OrgChart";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload this page.
+        </p>
+        <OrgChart></OrgChart>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
