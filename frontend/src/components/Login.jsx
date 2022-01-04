c2a09fa429d8265107bb56e4e30b18748f306c5c
Philip Suggars <phil@red-robot.biz>
2022-01-04 06:58:18 +0000

    WIP: building out scaffoldng for frontend

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,20 +1,7 @@
 import React, { useEffect, useState } from "react";
 import {
-    Box,
     Button,
-    Flex,
-    Input,
-    InputGroup,
-    Modal,
-    ModalBody,
-    ModalCloseButton,
-    ModalContent,
-    ModalFooter,
-    ModalHeader,
-    ModalOverlay,
-    Stack,
-    Text,
-    useDisclosure
+
 } from "@chakra-ui/react";
 
 const TodosContext = React.createContext({
