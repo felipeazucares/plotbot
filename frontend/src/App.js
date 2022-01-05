4d1a650a68d2bc54a0a7474bbc154576a34251aa
FelipeAzucares <phil@red-robot.biz>
2022-01-05 22:12:28 +0000

    Working: add login form into text component just for login purposes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -29,6 +29,7 @@ function App() {
           <div className="App-content-container-25">
             <div className="App-content-column">
               <TextContainer className="App-content-element"></TextContainer>
+              <LoginForm></LoginForm>
             </div>
           </div>
         </div>
