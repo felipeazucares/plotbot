c923a30a5884798f02a436fa6f1c0e6c9b61317d
FelipeAzucares <phil@red-robot.biz>
2022-01-07 21:28:21 +0000

    Working: removed axios refs and code

--- frontend/src/components/ButtonGetStoryTree.jsx
+++ frontend/src/components/ButtonGetStoryTree.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react"
-import axios from "axios"
 import {
     Button
 } from "@chakra-ui/react"
@@ -13,8 +12,7 @@ import {
             const response = await fetch("http://localhost:9000/story",
                 {
                     credentials:"include"
-                    // headers: {"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIkMmIkMTIka3NlTVR6ZnV6L1h3MjdtTnIxNjhKT1Z4UWVHL2hiVU8vMko2VDR2eGUvei9meVhZQUozVS4iLCJzY29wZXMiOltdLCJleHAiOjE2NDE1Mzk2MjJ9.IQbegXFRSt4LfeReCVUXJk6tkrYls2mhfKezK4bX-jY"},
-                })
+                 })
             if (response.status===200 && response.statusText==="OK"){
                 setStoryTree(response["data"])
                 console.log(await response["data"].json());
@@ -25,26 +23,6 @@ import {
         catch(error){
             console.error(`Exception occured getting story tree: ${error}`)
         }
-        // const baseURL= "http://localhost:9000/story"
-        // const axiosConfig={
-        //         method: 'GET',
-        //         url: baseURL,
-        //         withCredentials: true
-        // }
-        // try{            
-        //     const response = await axios(axiosConfig)
-
-        //     if (response.status===200 && response.statusText==="OK"){
-        //         setStoryTree(response["data"])
-        //         console.log(response["data"].json());
-        //     } else {
-        //         console.error(`get /story failed with status:${response.status} - ${response.statusText}`)
-        //         console.log(response);
-        //     }
-        // }
-        // catch(error){
-        //     console.error(`Exception occured getting story tree: ${error}`)
-        // }
     }
 
     const renderButtonGetStoryTree =(
