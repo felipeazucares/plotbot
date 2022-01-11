062d58c40b9b0ff2ab9c66a0d069a9cea9a28434
Philip Suggars <phil@red-robot.biz>
2022-01-11 18:13:52 +0000

    Working: generates 3 texts

--- frontend/src/components/OrgChart.jsx
+++ frontend/src/components/OrgChart.jsx
@@ -5,71 +5,33 @@ import { useCenteredTree } from "./Helpers";
 import { StoryTreeContext } from "../App"
 
 
-    // const [text,setText] = useState("")
-    // const payload={
-    //             "prompt": "It was cold outside, but not as cold as Peter felt in his heart.",
-    //             "temperature": 0.71234132
-    //         }
-    // const tryGetText = async () => {
-    //     try{            
-    //         const response = await fetch("http://localhost:9000/text",{method:"post", body: JSON.stringify(payload), credentials:"include", headers: {"Content-Type": "application/json"}})
-    //         if (response.status===200 && response.statusText==="OK"){
-    //             console.log("get text")
-    //             const result = await response.json()
-    //             console.log(`result:${JSON.stringify(result)}`)
-    //             setText(await result.data)
-    //             // setUser(username)
-    //         } else {
-    //             console.error(`generating text failed with status:${response.status} - ${response.statusText}`)
-    //         }
-    //     }
-    //     catch(error){
-    //         console.error(`Exception occured generating text: ${error}`)
-    //     }
-    //     console.log(`text:${JSON.stringify(text)}`);
-    //     // no that we have the text add it onto the the last item in the tree
-    //     try{            
-    //         const response = await fetch("http://localhost:9000/story/?parent_id=1976c33e-6c6e-11ec-b1ed-f01898e87167",{method:"post", body: JSON.stringify(text), credentials:"include", headers: {"Content-Type": "application/json"}})
-    //         if (response.status===200 && response.statusText==="OK"){
-    //             console.log("save text to db")
-    //             const result = await response.json()
-    //             console.log(`returned text: ${JSON.stringify(result)}`)
-    //             // setUser(username)
-    //         } else {
-    //             console.error(`save text failed with status:${response.status} - ${response.statusText}`)
-    //         }
-    //     }
-    //     catch(error){
-    //         console.error(`Exception occured saving text: ${error}`)
-    //     }
-    // }
-
-
-
 export default function OrgChartTree() {
   const {storyTree, setStoryTree} = useContext(StoryTreeContext)
   const [translate, containerRef] = useCenteredTree()
 
   const [text,setText] = useState("")
 
-  const payload={
-              "prompt": "It was cold outside, but not as cold as Peter felt in his heart.",
-              "temperature": 0.71234132
-          }
   const renderNodeWithCustomEvents = ({nodeDatum,toggleNode,handleNodeClick}) => (
-  <g>
+    <g>
     <circle r="10" onClick={() => handleNodeClick(nodeDatum)} />
-    <text fill="blue" strokeWidth="0" x="15" onClick={tryGetText}>
+    <text fill="blue" strokeWidth="0" x="15" onClick={toggleNode}>
       {nodeDatum.name}
     </text>
-    {nodeDatum.attributes?.text && (
-      <text fill="blue" x="20" y="20" strokeWidth="0">
+    {/* {nodeDatum.attributes?.text && (
+      <text fill="grey" x="20" y="20" strokeWidth="0">
         {nodeDatum.attributes?.text}
       </text>
-    )}
+    )} */}
   </g>
 );
-    const tryGetText = async () => {
+
+const tryGetText = async (parent_id,promptText) => {
+      const payload={
+                  "prompt": promptText,
+                  "temperature": 0.71234132
+              }
+      //get last sentence from text provided
+
       try{            
           const response = await fetch("http://localhost:9000/text",{method:"post", body: JSON.stringify(payload), credentials:"include", headers: {"Content-Type": "application/json"}})
           if (response.status===200 && response.statusText==="OK"){
@@ -88,7 +50,71 @@ export default function OrgChartTree() {
       console.log(`text:${JSON.stringify(text)}`);
       // no that we have the text add it onto the the last item in the tree
       try{            
-          const response = await fetch("http://localhost:9000/story/?parent_id=1976c33e-6c6e-11ec-b1ed-f01898e87167",{method:"post", body: JSON.stringify(text), credentials:"include", headers: {"Content-Type": "application/json"}})
+          const response = await fetch(`http://localhost:9000/story/?parent_id=${parent_id}`,{method:"post", body: JSON.stringify(text), credentials:"include", headers: {"Content-Type": "application/json"}})
+          if (response.status===200 && response.statusText==="OK"){
+              console.log("save text to db")
+              const result = await response.json()
+              console.log(`returned text: ${JSON.stringify(result)}`)
+              // setUser(username)
+          } else {
+              console.error(`save text failed with status:${response.status} - ${response.statusText}`)
+          }
+      }
+      catch(error){
+          console.error(`Exception occured saving text: ${error}`)
+      }
+
+        try{            
+          const response = await fetch("http://localhost:9000/text",{method:"post", body: JSON.stringify(payload), credentials:"include", headers: {"Content-Type": "application/json"}})
+          if (response.status===200 && response.statusText==="OK"){
+              console.log("get text")
+              const result = await response.json()
+              console.log(`result:${JSON.stringify(result)}`)
+              setText(await result.data)
+              // setUser(username)
+          } else {
+              console.error(`generating text failed with status:${response.status} - ${response.statusText}`)
+          }
+      }
+      catch(error){
+          console.error(`Exception occured generating text: ${error}`)
+      }
+      console.log(`text:${JSON.stringify(text)}`);
+      // no that we have the text add it onto the the last item in the tree
+      try{            
+          const response = await fetch(`http://localhost:9000/story/?parent_id=${parent_id}`,{method:"post", body: JSON.stringify(text), credentials:"include", headers: {"Content-Type": "application/json"}})
+          if (response.status===200 && response.statusText==="OK"){
+              console.log("save text to db")
+              const result = await response.json()
+              console.log(`returned text: ${JSON.stringify(result)}`)
+              // setUser(username)
+          } else {
+              console.error(`save text failed with status:${response.status} - ${response.statusText}`)
+          }
+      }
+      catch(error){
+          console.error(`Exception occured saving text: ${error}`)
+      }
+
+        try{            
+          const response = await fetch("http://localhost:9000/text",{method:"post", body: JSON.stringify(payload), credentials:"include", headers: {"Content-Type": "application/json"}})
+          if (response.status===200 && response.statusText==="OK"){
+              console.log("get text")
+              const result = await response.json()
+              console.log(`result:${JSON.stringify(result)}`)
+              setText(await result.data)
+              // setUser(username)
+          } else {
+              console.error(`generating text failed with status:${response.status} - ${response.statusText}`)
+          }
+      }
+      catch(error){
+          console.error(`Exception occured generating text: ${error}`)
+      }
+      console.log(`text:${JSON.stringify(text)}`);
+      // no that we have the text add it onto the the last item in the tree
+      try{            
+          const response = await fetch(`http://localhost:9000/story/?parent_id=${parent_id}`,{method:"post", body: JSON.stringify(text), credentials:"include", headers: {"Content-Type": "application/json"}})
           if (response.status===200 && response.statusText==="OK"){
               console.log("save text to db")
               const result = await response.json()
@@ -105,7 +131,7 @@ export default function OrgChartTree() {
 
   const handleNodeClick = (nodeDatum) => {
     if (nodeDatum.children.length===0){
-      tryGetText()
+      tryGetText(nodeDatum._id,nodeDatum.attributes.text)
     }
 }
 
