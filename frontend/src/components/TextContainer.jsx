3d4f0cd80043186b01282ba10997173d0b53248b
Philip Suggars <phil@red-robot.biz>
2022-01-13 20:32:44 +0000

    Working: added login code to load of screen

--- frontend/src/components/TextContainer.jsx
+++ frontend/src/components/TextContainer.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState, useEffect} from "react"
-import { StoryTreeContext,StoryTextContext } from "../App"
+import { StoryTreeContext,StoryTextContext,UserContext } from "../App"
 import { Container } from '@chakra-ui/react'
 
 export default function TextContainer() {
@@ -7,8 +7,47 @@ export default function TextContainer() {
     const {storyTree, setStoryTree} = useContext(StoryTreeContext)
 
     // go and get the story from the API
-
+    
+    // const [password, setPassword] = useState("")
+    const [username, setUsername] = useState("")
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const {user,setUser} = useContext(UserContext)
+    
     useEffect(() => {
+
+    // const noUsernameError = username === ""
+    // const noPasswordError = password === ""
+    //get user from context
+    
+    // const handleUsernameChange = (inputValue) => setUsername(inputValue.target.value)
+    // const handlePasswordChange = (inputValue) => setPassword(inputValue.target.value)
+
+    const tryLogin = async (event) => 
+    {
+        // don"t reload the page
+        event.preventDefault()
+        // now cook up the form
+        let formData = new FormData()
+        formData.append("grant_type","password")
+        formData.append("scope","story:reader story:writer")
+        formData.append("username","felipeazucares")
+        formData.append("password","BlackM1lk")
+        try{            
+            const response = await fetch("http://localhost:9000/login",{method:"POST", body: formData, credentials:"include"})
+            if (response.status===200 && response.statusText==="OK"){
+                console.log("logged in successfully")
+                setIsLoggedIn(true)
+                setUser(username)
+                
+            } else {
+                console.error(`Login failed with status:${response.status} - ${response.statusText}`)
+            }
+        }
+        catch(error){
+            console.error(`Exception occured loggin in: ${error}`)
+        }
+    }    
+
     const tryGetStoryText = async () => 
     {
         try{            
