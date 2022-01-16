e501f3192c593dc83d36dbab5eddf54a4cedb8b2
FelipeAzucares <phil@red-robot.biz>
2022-01-16 10:34:35 +0000

    WIP: remapped backend port to 8450 for compatibility with Okteto deployment

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("api.api:app", host="0.0.0.0", port=9000, reload=True)
+    uvicorn.run("api.api:app", host="0.0.0.0", port=8450, reload=True)
