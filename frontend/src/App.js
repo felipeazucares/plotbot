e7e4a783b49dae1497c0b10fdc77a092ddbc2e8c
Philip Suggars <phil@red-robot.biz>
2022-01-18 17:29:51 +0000

    WIP: added in urlcontext to manage target URL across components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,11 @@ import TextContainer from "./components/TextContainer";
 // import OrgChart from "./components/OrgChart"
 import Header from "./components/Header";
 
+export const URLContext = React.createContext({
+  baseAPIURL: "",
+  setURL: () => {},
+});
+
 export const UserContext = React.createContext({
   user: "",
   setUser: () => {},
@@ -28,6 +33,7 @@ export const TemperatureContext = React.createContext({
 });
 
 function App() {
+  const [baseAPIURL, setURL] = useState("http://localhost:8450");
   const [user, setUser] = useState("not logged in");
   const value = { user, setUser };
   // this will contain the tree representation
@@ -36,7 +42,9 @@ function App() {
   // this will set the text generated from the tree
   const [storyTree, setStoryTree] = useState({
     name: "Something broke!",
-    attributes: { text: "Oops! Check you're logged in!" },
+    attributes: {
+      text: "Oops! Check you're logged in by clicking the Start button!",
+    },
   });
 
   const tree = { storyTree, setStoryTree };
@@ -47,28 +55,30 @@ function App() {
       <StoryTreeContext.Provider value={tree}>
         <StoryTextContext.Provider value={text}>
           <TemperatureContext.Provider value={temp}>
-            <div className="App">
-              <div className="App-header">
-                <Header />
-              </div>
-              {/* <Divider></Divider> */}
-              <div className="App-row-container">
-                <div className="App-content-container-75">
-                  <div className="App-content-column">
-                    <TreeContainer className="App-content-element"></TreeContainer>
+            <URLContext.Provider value={baseAPIURL}>
+              <div className="App">
+                <div className="App-header">
+                  <Header />
+                </div>
+                {/* <Divider></Divider> */}
+                <div className="App-row-container">
+                  <div className="App-content-container-75">
+                    <div className="App-content-column">
+                      <TreeContainer className="App-content-element"></TreeContainer>
+                    </div>
+                  </div>
+                  <div className="App-content-container-25">
+                    <div className="App-content-column">
+                      <TextContainer className="App-content-element"></TextContainer>
+                    </div>
                   </div>
                 </div>
-                <div className="App-content-container-25">
-                  <div className="App-content-column">
-                    <TextContainer className="App-content-element"></TextContainer>
-                  </div>
+                <div>
+                  <SliderContainer></SliderContainer>
+                  <ControlsContainer></ControlsContainer>
                 </div>
               </div>
-              <div>
-                <SliderContainer></SliderContainer>
-                <ControlsContainer></ControlsContainer>
-              </div>
-            </div>
+            </URLContext.Provider>
           </TemperatureContext.Provider>
         </StoryTextContext.Provider>
       </StoryTreeContext.Provider>
