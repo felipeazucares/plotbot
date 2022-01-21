6158da545fb8c0b717b727b39fe7223fe15df0a2
Philip Suggars <phil@red-robot.biz>
2022-01-21 08:02:08 +0000

    WIP: added okteto.yml stack file

--- buildprod.sh
+++ buildprod.sh
@@ -1,9 +1,12 @@
 cd ~/sites/plotbot/frontend
-docker build -f Dockerfile.prod -t felipeazucares/hp-ui .
-docker push felipeazucares/hp-ui
-cd ../backend
-docker build -t felipeazucares/hp-api .
-docker push felipeazucares/hp-api
+# need to swap the files here as okteto stacks don't let you specify a dockerfile
+cp Dockerfile.prod Dockerfile
+# not needed as Okteto --build builds and pushes the images
+# docker build -f Dockerfile -t felipeazucares/hp-ui .
+# docker push felipeazucares/hp-ui
+# cd ../backend
+# docker build -t felipeazucares/hp-api .
+# docker push felipeazucares/hp-api
 cd ..
 okteto context create
 okteto stack deploy --build
